64e89f48d78ac8cabbbc019e7eab970cb4be1ce1
victorrobuschi <victor.robuschi@hotmail.com>
2021-01-24 12:19:19 +0100

    first list walkthro and exercises

--- Python_crash_course/Part_1_Basics/Chapter_3_Introducing_Lists/motorcycles.py
+++ Python_crash_course/Part_1_Basics/Chapter_3_Introducing_Lists/motorcycles.py
@@ -1,12 +1,59 @@
-# Changing, adding and removing elements in a list
+# Changing, adding and removing items in a list
 
-# Modifying element
+# Modifying item
 motorcycles = ['honda', 'yamaha', 'suzuki']
 print(motorcycles)
 motorcycles[0] = 'ducati'
 print(motorcycles)
 
-# Addding element to the end of a list
+# Addding item to the end of a list
 motorcycles.append('ducati')
 print(motorcycles)
 
+# You could also build up a list from scratch with append()
+motorcycles = []
+
+motorcycles.append('harley davidsson')
+motorcycles.append('BMW')
+motorcycles.append('ducati')
+
+print(motorcycles)
+# If you want to put in a item in a specific position, use intert()
+motorcycles.insert(0, 'honda')
+
+print(motorcycles)
+# To delete a item from the list, use "del". you have to pick a specific item
+del motorcycles[0]
+
+print(motorcycles)
+
+# with "del" you remove a item completely and you wont be able to use it afterwards
+# if you still want to work with a removed item then we use pop()
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
+
+# popping any item in a list. 
+motorcycles = ['honda', 'yamaha', 'suzuki']
+first_owned = motorcycles.pop(0)
+print('The first motorcycle I owned was a ' + first_owned.title() + '.') 
+
+# Removing item by value with remove(value)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+
+# if we want to work with the removed item we save it in a varabile
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
---
+++ Try_it_yourself/Chapter_3/3_4_guest_list.py
@@ -0,0 +1,103 @@
+""" 3-4. Guest List: If you could invite anyone, living or deceased, to dinner, who
+would you invite? Make a list that includes at least three people you’d like to
+invite to dinner. Then use your list to print a message to each person, inviting
+them to dinner. """
+
+guest_list = ['simone de beauvoir', 'albert einstein', 'ragnar lothbrock']
+
+print('Dear ' + guest_list[0].title() + ' i hereby would like to extend a dinner invitation to you.')
+print('Dear ' + guest_list[1].title() + ' i hereby would like to extend a dinner invitation to you.')
+print('Dear ' + guest_list[2].title() + ' i hereby would like to extend a dinner invitation to you.')
+
+""" 3-5. Changing Guest List: You just heard that one of your guests can’t make the
+dinner, so you need to send out a new set of invitations. You’ll have to think of
+someone else to invite.
+
+• Start with your program from Exercise 3-4. Add a print statement at the
+end of your program stating the name of the guest who can’t make it.
+
+• Modify your list, replacing the name of the guest who can’t make it with
+the name of the new person you are inviting.
+
+• Print a second set of invitation messages, one for each person who is still
+in your list. """
+
+print('-----------------------------------------------------------------')
+
+print('Unfortunately, ' + guest_list[2].title() + ' could not make it for dinner.')
+
+guest_list[2] = 'harald hardrada'
+
+print('\nDear ' + guest_list[0].title() + ' i hereby would like to extend a dinner invitation to you.')
+print('Dear ' + guest_list[1].title() + ' i hereby would like to extend a dinner invitation to you.')
+print('Dear ' + guest_list[2].title() + ' i hereby would like to extend a dinner invitation to you.')
+
+print('-----------------------------------------------------------------')
+
+""" 3-6. More Guests: You just found a bigger dinner table, so now more space is
+available. Think of three more guests to invite to dinner.
+
+• Start with your program from Exercise 3-4 or Exercise 3-5. 
+Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+• Use insert() to add one new guest to the beginning of your list.
+
+• Use insert() to add one new guest to the middle of your list.
+
+• Use append() to add one new guest to the end of your list.
+
+• Print a new set of invitation messages, one for each person in your list """
+
+print('\nEsteemed guests, we have found an even bigger dinner table and will be inviting 3 more guests.')
+# guest_list = ['simone de beauvoir', 'albert einstein', 'harald hardrada']
+
+guest_list.insert(1, 'pope pius XII')
+guest_list.insert(3, 'arthur patschke')
+guest_list.append('harold godwinson')
+
+print('\nDear ' + guest_list[1].title() + ' i hereby would like to extend a dinner invitation to you.')
+print('Dear ' + guest_list[3].title() + ' i hereby would like to extend a dinner invitation to you.')
+print('Dear ' + guest_list[5].title() + ' i hereby would like to extend a dinner invitation to you.')
+
+print('\n-----------------------------------------------------------------')
+
+""" 3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, 
+and you have space for only TWO guests.
+
+• Start with your program from Exercise 3-6. Add a new line that prints a
+message saying that you can invite only two people for dinner.
+
+• Use pop() to remove guests from your list one at a time until only two
+names remain in your list. Each time you pop a name from your list, print
+a message to that person letting them know you’re sorry you can’t invite
+them to dinner.
+
+• Print a message to each of the two people still on your list, letting them
+know they’re still invited.
+
+• Use del to remove the last two names from your list, so you have an empty
+list. Print your list to make sure you actually have an empty list at the end
+of your program. """
+# ['simone de beauvoir', 'pope pius XII', 'albert einstein', 'arthur patschke', 'harald hardrada', 'harold godwinson']
+
+print('\nDear guests I regret to inform you that new dinner table wont arrive in time for the dinner and we can only arrange seeting for two guests.')
+
+removed_guest = guest_list.pop(0)
+print("\n" + removed_guest.title() + "I'm sorry to inform you that you have been uninvited to the dinner, sucks to suck...")
+
+removed_guest = guest_list.pop(0)
+print(removed_guest.title() + "I'm sorry to inform you that you have been uninvited to the dinner, sucks to suck...")
+
+removed_guest = guest_list.pop(0)
+print(removed_guest.title() + "I'm sorry to inform you that you have been uninvited to the dinner, sucks to suck...")
+
+removed_guest = guest_list.pop(0)
+print(removed_guest.title() + "I'm sorry to inform you that you have been uninvited to the dinner, sucks to suck...")
+
+print('\n' + guest_list[0].title() + 'you are still invited to the dinner, hope to see you there.')
+print(guest_list[1].title() + 'you are still invited to the dinner, hope to see you there.')
+
+del guest_list[0]
+del guest_list[0]
+
+print(guest_list)
